bf7b8a572aba98e7782ec033aa5edaf672dc6f1a
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-04-21 14:09:31 -0500

    created event model

---
+++ db/migrate/20230421190902_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.string :location
+      t.datetime :start_time
+      t.datetime :end_time
+      t.references :interest, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
