cd44110f1ee47e5adeed995eca65538e9b1d2374
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-05-06 15:02:41 -0500

    remove the comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,4 @@
 class Event < ApplicationRecord
-  # An Event can have multiple Users attending.
-  # An Event belongs to an Interest (category).
-  # An Event has many UserEvents (the join model).
-  # An Event has many Users through UserEvents.
   has_many :user_events, dependent: :destroy
   has_many :users, through: :user_events
   has_many :reviews, dependent: :destroy
--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,7 +1,4 @@
 class UserEvent < ApplicationRecord
-  # UserEvent is the join model that connects Users and Events.
-  # A UserEvent belongs to a User.
-  # A UserEvent belongs to an Event.
   belongs_to :user
   belongs_to :event
 end
