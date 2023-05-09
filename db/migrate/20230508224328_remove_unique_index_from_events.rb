82f56f6ef756f8bf560d84bebbd8452fb869b64b
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-05-08 23:52:52 -0500

    fix the migration issue

--- db/migrate/20230508224328_remove_unique_index_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUniqueIndexFromEvents < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :events, column: [:name, :start_time], unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_224328) do
+ActiveRecord::Schema.define(version: 2023_05_05_183125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2023_05_08_224328) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_user_events_on_event_id"
     t.index ["user_id"], name: "index_user_events_on_user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
